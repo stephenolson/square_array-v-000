ba2125d734eafbf2e101cb848608a7963f2c8b7f
Stephen Olson <stephenolson@gmail.com>
2018-09-19 22:33:50 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-    new_array = []
-    array.each do |element|
-        new_array << element ** 2
-    end
+  new_array = []
+  array.each do |i|
+    new_array << i ** 2
+  end
     new_array
 end
